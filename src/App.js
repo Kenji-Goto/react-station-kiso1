4d59af049f7cb02efd3bf3bd5f7c5395d4d8b530
Kenji0707 <yugetilya.kunn@gmail.com>
2023-08-17 17:48:15 +0900

    [finish] Station01

--- src/App.js
+++ src/App.js
@@ -5,19 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 className='sign'>掲示板</h2>
+        <h2 className='make'>スレッドを作成</h2>
       </header>
+      <body>
+        予定を表示
+      </body>
     </div>
   );
 }
