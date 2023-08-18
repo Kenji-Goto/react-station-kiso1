6781d74b9aabd68bf8971e61eaa1d3a9e1006e4b
Kenji0707 <yugetilya.kunn@gmail.com>
2023-08-18 16:27:30 +0900

    [add] ルーティングのためのmain.js

--- src/threads.js
+++ src/threads.js
@@ -1,3 +1,16 @@
 import axios from 'axios'
 
-const 
+
+export function Threads(){
+axios.get("https://virtserver.swaggerhub.com/INFO_3/BulletinBoardApplication/1.0.0/threads?offset=1")
+.then((response) => {
+    console.log(response.data)
+})
+.catch((error)=>console.log(error))
+
+
+return(
+    <p>新着スレッド</p>
+)
+
+}
